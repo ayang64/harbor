37030d0ad027f85e4d2f670021719ce665a24607
Ayan George <ayan@ayan.net>
2019-12-04 23:03:58 -0500

    point check-in

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/rand"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -9,15 +10,24 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/ayang64/harbor/jail"
 )
 
 type FuncMap map[string]func(...string) error
 type Dispatcher struct {
+	root    string
 	log     *log.Logger
 	debug   *log.Logger
 	funcmap FuncMap
+}
+
+func WithRoot(r string) func(*Dispatcher) error {
+	return func(d *Dispatcher) error {
+		d.root = r
+		return nil
+	}
 }
 
 func WithLogWriter(w io.Writer) func(*Dispatcher) error {
@@ -49,55 +59,36 @@ func NewDispatcher(opts ...func(*Dispatcher) error) (*Dispatcher, error) {
 			return nil, err
 		}
 	}
+
 	return &rc, nil
 }
 
 func main() {
+	log.SetFlags(log.Llongfile | log.LstdFlags)
 	if len(os.Args) < 2 {
 		log.Fatalf("%s run <cmd>", os.Args[0])
 	}
+
+	fs := flag.NewFlagSet("run", flag.ExitOnError)
+
 	switch os.Args[1] {
 	case "run":
-		if err := run(os.Args[2], os.Args[3:]); err != nil {
+		root := fs.String("root", "./freebsd", "path of jail root")
+		fs.Parse(os.Args[2:])
+		log.Printf("args = %#v", fs.Args())
+		abspath, err := filepath.Abs(*root)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := run(abspath, fs.Args()); err != nil {
 			log.Fatal(err)
 		}
 	}
 }
 
-func run(root string, args []string) error {
-	// set current working directory to our new root
-	if err := os.Chdir(root); err != nil {
-		return err
-	}
+func run(root string, cmd []string) error {
 
-	rndhost := func() string {
-		// store 128 random bits
-		buf := make([]byte, 16, 16)
-		rand.Read(buf)
-		return fmt.Sprintf("%x", buf)
-	}
-
-	id, err := jail.Set(jail.CREATE|jail.ATTACH,
-		"path", "/home/ayan/jailroot",
-		"name", "foobar",
-		"host.domainname", "ayan.net",
-		"ip4.addr", net.ParseIP("192.168.0.18").To4(),
-		"allow.raw_sockets", true,
-		"children.max", 10,
-		"enforce_statfs", 1,
-		"allow.socket_af", true,
-		"allow.mount", true,
-		"allow.mount.devfs", true,
-		"allow.mount.procfs", true,
-		"host.hostname", rndhost())
-
-	if err != nil {
-		return err
-	}
-
-	log.Printf("created jail #%d", id)
-
-	d, err := NewDispatcher(WithDebugWriter(os.Stderr))
+	d, err := NewDispatcher(WithDebugWriter(os.Stderr), WithRoot(root))
 
 	d.debug.Printf("debuging output is enabled.")
 
@@ -124,7 +115,40 @@ func (d *Dispatcher) dispatch(args ...string) error {
 }
 
 func (d *Dispatcher) run(args ...string) error {
+	// set current working directory to our new root
+	if err := os.Chdir(d.root); err != nil {
+		return err
+	}
+
+	rndhost := func() string {
+		// store 128 random bits
+		buf := make([]byte, 16, 16)
+		rand.Read(buf)
+		return fmt.Sprintf("%x", buf)
+	}
+
 	// build sub-command execution context.
+	id, err := jail.Set(jail.CREATE|jail.ATTACH,
+		"path", d.root,
+		"name", "foobar",
+		"host.domainname", "ayan.net",
+		"ip4.addr", net.ParseIP("192.168.0.18").To4(),
+		"allow.raw_sockets", true,
+		"children.max", 10,
+		"enforce_statfs", 1,
+		"allow.socket_af", true,
+		"allow.mount", true,
+		"allow.mount.devfs", true,
+		"allow.mount.procfs", true,
+		"host.hostname", rndhost())
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("root = %s", d.root)
+	log.Printf("created jail #%d", id)
+
 	cmd := exec.Command(args[0], args[1:]...)
 
 	// setup stdout, stderr, and stdin for our sub-process.
